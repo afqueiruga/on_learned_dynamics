ee6b1e8793321ab6c31bf1454fcaa69f82e7dd86
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2019-10-18 15:43:43 -0700

    still looking at the nonlinear pendulum

--- ode_helper.py
+++ ode_helper.py
@@ -1,3 +1,26 @@
 import torch
 import torchdiffeq
 
+def train_a_neural_ode(data, ts, model=None, batch_size=25, n_future=1, 
+                       learning_rate = 1.0e-4, N_iter = 50000,
+                       verbose=False, device=None, method='euler'):
+    if device is None:
+        device = get_device()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    losses = []
+    N_print, N_trace = N_iter, 100
+    for itr in range(1, N_iter):
+        optimizer.zero_grad()
+        batch_y0, batch_t, batch_y = get_batch(data, ts,
+                                               batch_size, n_future)
+        pred_y = torchdiffeq.odeint(model, batch_y0, batch_t,
+                                    method=method)
+        loss = torch.mean(torch.abs(pred_y - batch_y))
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.detach().numpy())
+        if itr % 1000 == 0:
+            with torch.no_grad():
+                pred_y = torchdiffeq.odeint(model, batch_y0, batch_t)
+                loss = torch.mean(torch.abs(pred_y - batch_y))
+    return model,np.array(losses)
